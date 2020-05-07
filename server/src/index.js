c16636583020b9c0e25ad1d5b98198098ac84ef1
Bruno Guerra <gguerrabruno@outlook.com>
2020-05-07 17:25:49 -0300

    Initial commit

---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const PORT = 3333
+const HOST = '0.0.0.0'
+
+app.get('/', (req, res) => {
+    res.send("Hello world!")
+})
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server started at ${PORT}`)
+})
