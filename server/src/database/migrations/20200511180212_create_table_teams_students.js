38640de454d16a914e3d450a7fe0e5f2dc7028e4
Bruno Guerra <gguerrabruno@outlook.com>
2020-05-13 14:36:21 -0300

    Table creation

---
+++ server/src/database/migrations/20200511180212_create_table_teams_students.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('teams_students', (table) => {
+        table.increments('id')
+        table.integer('student').unsigned().notNullable();
+        table.foreign('student').references('id').inTable('students');
+        table.integer('team').unsigned().notNullable();
+        table.foreign('team').references('id').inTable('teams');
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('teams_students')
+};
