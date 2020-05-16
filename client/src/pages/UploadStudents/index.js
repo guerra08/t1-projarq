adc0e0118676251aed9fd838c94943747a636249
rizzotto <guilherme.rizzotto@acad.pucrs.br>
2020-05-16 15:36:40 -0300

    upload list

--- client/src/pages/UploadStudents/index.js
+++ client/src/pages/UploadStudents/index.js
@@ -2,6 +2,13 @@ import React, { useState } from 'react'
 
 import './styles.css'
 
+import avatar0 from '../../assets/avatar0.svg'
+import avatar1 from '../../assets/avatar1.svg'
+import avatar2 from '../../assets/avatar2.svg'
+import avatar3 from '../../assets/avatar3.svg'
+import avatar4 from '../../assets/avatar4.svg'
+import disabledSvg from '../../assets/disabled.svg'
+
 export default function UploadStudents() {
   const [students, setStudents] = useState([])
   //not working
@@ -9,24 +16,49 @@ export default function UploadStudents() {
     let file = e.target.files[0]
     const reader = new FileReader()
     reader.onload = async function () {
-      // console.log()
-      await setStudents([JSON.parse(reader.result)])
+      // console.log(JSON.parse(reader.result)[0])
+      await setStudents([JSON.parse(reader.result).data])
     }
     reader.readAsText(file)
   }
 
+  function getRandomSvg() {
+    let vector = [avatar0, avatar1, avatar2, avatar3, avatar4]
+    return vector[Math.floor(Math.random() * 4)]
+  }
+
   return (
-    <div className="updateContainer">
-      {console.log(students)}
-      <input type="file" name="file" onChange={(e) => handleChange(e)}></input>
-      <ul>
-        {students.map((student) => (
-          <li>
-            <p>{student.name}</p>
-            <p>{student.course}</p>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <p>Escolha um arquivo</p>
+      <div className="updateContainer">
+        <input
+          className="inputButton"
+          type="file"
+          name="file"
+          onChange={(e) => handleChange(e)}
+        ></input>
+        <ul className="listUpload">
+          {students.length === 0 ? (
+            <> </>
+          ) : (
+            students[0].map((student) => (
+              <li className="l" key={student.id}>
+                <div className="insideButton">
+                  {/* {console.log(team.avatar)} */}
+                  {/* alterar com dados da api */}
+                  <img src={getRandomSvg()} alt="team"></img>
+                  <div>
+                    <p>
+                      <strong>{student.name}</strong>
+                    </p>
+                    <p>{student.course_name}</p>
+                  </div>
+                </div>
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
     </div>
   )
 }
