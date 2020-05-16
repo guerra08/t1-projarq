2b458a766b8aa30361ee1da324bc148045937db1
Bruno Guerra <gguerrabruno@outlook.com>
2020-05-16 20:51:47 -0300

    Starting to build login

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,29 +1,36 @@
 import React, { useState } from 'react'
-// import { Link, useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi'
 
-// import api from '../../services/api'
+import api from '../../services/api'
 
 import './styles.css'
 
 export default function Login() {
-  const [id, setId] = useState('')
+  const [code, setCode] = useState('')
   const [userType, setUserType] = useState('')
-  // const history = useHistory()
+  const history = useHistory()
 
   async function handleLogin(e) {
     e.preventDefault()
-
-    // try {
-    //   const response = await api.post('sessions', { id, userType })
-
-    //   localStorage.setItem('userId', id)
-    //   localStorage.setItem('userName', response.data.name)
-
-    //   history.push('/anotherpage')
-    // } catch (err) {
-    //   alert('Falha no login, tente novamente.')
-    // }
+    try {
+      const res = await api.post(`/${userType}/login`, { code })
+      localStorage.setItem('userId', `${res.data.id}`)
+      localStorage.setItem('userType', `${userType}`)
+      localStorage.setItem('name', `${res.data.name}`)
+      if(userType === 'students'){
+        history.push('/create')
+      }
+      else if(userType === 'professors'){
+        history.push('/evaluate')
+      }
+      else if(userType === 'admin'){
+        //Admin routes
+      }
+    } catch (error) {
+      console.log(error)
+      alert('Falha no logon, tente novamente!')
+    }
   }
 
   async function setUser(e) {
@@ -38,19 +45,23 @@ export default function Login() {
 
           <input
             className="input"
-            placeholder="Seu ID"
-            value={id}
-            onChange={(e) => setId(e.target.value)}
+            placeholder="Seu código"
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
           />
 
           <div className="option" onChange={(e) => setUser(e)}>
-            <div className="professor">
-              <input type="radio" value="professor" name="option" />
+            <div className="professors">
+              <input type="radio" value="professors" name="option" />
               <p className="color">Professor</p>
             </div>
-            <div className="student">
-              <input type="radio" value="student" name="option" />
+            <div className="students">
+              <input type="radio" value="students" name="option" />
               <p className="color">Aluno</p>
+            </div>
+            <div className="admins">
+              <input type="radio" value="admins" name="option" />
+              <p className="color">Admin</p>
             </div>
           </div>
 
