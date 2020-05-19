3b7c25229e29100100d08418dc8e5710bc4560e9
rizzotto <guilherme.rizzotto@acad.pucrs.br>
2020-05-19 17:40:01 -0300

    already created team validantion css

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navbar } from 'react-bootstrap'
-import { GrUserAdmin, GrUser } from 'react-icons/gr'
+import { FaUserTie, FaUser } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import Logout from '../Logout'
 import './styles.css'
@@ -8,7 +8,11 @@ import './styles.css'
 export default function ({ path, name, type }) {
   return (
     <Navbar fixed="top" expand="lg" variant="light">
-      {(type === 'admins') ? <GrUserAdmin size={30} className="icon"/> : <GrUser size={30} className="icon"/>}
+      {type === 'admins' ? (
+        <FaUserTie size={30} className="icon" />
+      ) : (
+        <FaUser size={30} className="icon" />
+      )}
       <Link className="link" to={`/${path}`}>
         {name}
       </Link>
