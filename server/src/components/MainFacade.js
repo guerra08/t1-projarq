27d25e34eadd451126adae248743d6711252305b
Bruno Guerra <gguerrabruno@outlook.com>
2020-07-01 17:51:56 -0300

    Main facade on backend

---
+++ server/src/components/MainFacade.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = require('../routes')
+const cors = require('cors')
+const PORT = 3333
+const HOST = '0.0.0.0'
+
+module.exports = {
+    startServer() {
+        const app = express()
+        app.use(cors())
+        app.use(express.json())
+        app.use(router)
+        app.listen(PORT, HOST, () => {
+            console.log(`Server started at ${PORT}`)
+        })
+    }
+}
